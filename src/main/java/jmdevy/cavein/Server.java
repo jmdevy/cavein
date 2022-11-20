8c2ca3a0f9a8177b195bb1179baf90fab079a3d9
jmdevy <marcumjp@gmail.com>
2022-11-20 15:34:46 -0500

    Pick random blocks to fall during cave in and not before

--- src/main/java/jmdevy/cavein/Server.java
+++ src/main/java/jmdevy/cavein/Server.java
@@ -18,24 +18,22 @@ import net.minecraftforge.server.ServerLifecycleHooks;
 
 import java.util.List;
 import java.util.Random;
+import java.util.Vector;
 import java.util.function.Supplier;
 
 @Mod.EventBusSubscriber(modid = Cavein.MODID, bus = Mod.EventBusSubscriber.Bus.FORGE, value = Dist.DEDICATED_SERVER)
 public class Server {
     private static Random random = new Random(System.currentTimeMillis());
 
-    private static final int caveinSizeXZ = 32;     // Really double since will look for blocks in -caveSizeXZ to +caveSizeXZ
-    private static final int caveinSizeAbove = 16;  // Just above the Y position the cave in starts at
+    private static final int caveinSizeAbove = 16;   // Just below the Y position the cave in starts at
     private static final int caveinSizeBelow = 4;   // Just below the Y position the cave in starts at
 
     private static long lastCaveinTickCount = 0;    // General timer
     private static long currentCaveinTickCount = 0; // General timer
 
-    private static BlockPos[] caveinBlockPositions = new BlockPos[256]; // Up to 256 blocks can be chosen to fall, makes sense to store and track so not always fetching chunks and block states
-    private static FallingBlockEntity[] entities = new FallingBlockEntity[256];
-    private static int caveinBlockPositionsIndex = 0;                   // The index up to the end of how many blocks were chosen to fall.
-    private static int entitiesIndex = 0;
+    private static List<FallingBlockEntity> entities = new Vector<FallingBlockEntity>();
 
+    private static Vec3 caveinOrigin;
     private static List<String> whitelistedBlocks;
 
 
@@ -106,54 +104,8 @@ public class Server {
                         // Generate random cave in attribute
                         caveinDurationSeconds = minSeconds + random.nextInt(maxSeconds - minSeconds + 1);
 
-                        // Store list of blocks to fall
                         ServerPlayer caveinPlayer = players.get(random.nextInt(players.size()));
-                        Vec3 caveinOrigin = caveinPlayer.position();
-                        caveinBlockPositionsIndex = 0;
-                        entitiesIndex = 0;
-
-                        int y = ((int)caveinOrigin.y) - caveinSizeBelow;
-
-                        // Find and store block positions for cave in
-                        for(int i=0; i<caveinBlockPositions.length*2; i++){
-                            // Generate random points in circle
-                            double r = ServerConfigHandler.SERVER_CONFIG.caveinRadius.get() * Math.sqrt(random.nextFloat());
-                            double theta = random.nextFloat() * 2 * Math.PI;
-                            int x = (int)(caveinOrigin.x + r * Math.cos(theta));
-                            int z = (int)(caveinOrigin.z + r * Math.sin(theta));
-
-                            // Serach bottom to top of cave in circle for blocks that have air or water under them
-                            for(int iyx=0; iyx<caveinSizeBelow+caveinSizeAbove; iyx++){
-                                if(iyx+y < ServerConfigHandler.SERVER_CONFIG.maxCaveinYLevel.get()) {
-                                    BlockPos block = new BlockPos(x, iyx + y, z);
-                                    String blockStr = event.getServer().overworld().getBlockState(block).getBlock().toString();
-
-                                    BlockPos blockBelow = new BlockPos(x, iyx + y - 1, z);
-                                    Material blockBelowMat = event.getServer().overworld().getBlockState(blockBelow).getMaterial();
-
-                                    if (checkIfWhitelisted(blockStr) && (blockBelowMat == Material.AIR || blockBelowMat == Material.WATER)) {
-                                        caveinBlockPositions[caveinBlockPositionsIndex] = new BlockPos(x, iyx + y, z);
-                                        caveinBlockPositionsIndex++;
-                                        break;
-                                    }
-                                }else{
-                                    break;
-                                }
-                            }
-
-                            // Full, can't fit anymore blocks to fall, stop looking
-                            if(caveinBlockPositionsIndex >= caveinBlockPositions.length){
-                                break;
-                            }
-                        }
-
-                        // Everything is setup for the cave in, tell the clients about to shake their cameras and play sound
-                        sendMessageToAllPlayers(players, new ToClientMessageCaveinStatus(
-                                true,
-                                (int)caveinOrigin.x,
-                                (int)caveinOrigin.y,
-                                (int)caveinOrigin.z,
-                                ServerConfigHandler.SERVER_CONFIG.caveinRadius.get()));
+                        caveinOrigin = caveinPlayer.position();
                     }
                 }
 
@@ -166,10 +118,12 @@ public class Server {
 
     private static void doCavein(TickEvent.ServerTickEvent event){
         if(event.phase == TickEvent.Phase.END) {
-            for(int iex=0; iex<entitiesIndex; iex++){
-                if(entities[iex] != null && entities[iex].isRemoved()){
-                    entities[iex].playSound(SoundEvents.STONE_FALL);
-                    entities[iex] = null;
+            // Go through and play sounds for blocks that landed, remove them too
+            for(int iex=0; iex<entities.size(); iex++){
+                FallingBlockEntity entity = entities.get(iex);
+                if(entity.isRemoved()){
+                    entity.playSound(SoundEvents.STONE_FALL);
+                    entities.remove(iex);
                 }
             }
 
@@ -177,36 +131,33 @@ public class Server {
             long currentCaveinCheckSecond = currentCaveinTickCount / Cavein.ticksPerSecond;
 
             // If cave in still happening, make blocks fall, otherwise, go back to checking
-            if(currentCaveinCheckSecond-lastCaveinCheckSecond < caveinDurationSeconds && caveinBlockPositionsIndex > 0){
+            if(currentCaveinCheckSecond-lastCaveinCheckSecond < caveinDurationSeconds){
                 Cavein.LOGGER.debug(String.valueOf(caveinDurationSeconds));
 
-                for(int ifx=0; ifx<1; ifx++){
-                    int fallingBlockIndex = random.nextInt(caveinBlockPositionsIndex);
-                    BlockPos currentBlockPos = caveinBlockPositions[fallingBlockIndex];
+                // Generate random point in circle
+                double r = ServerConfigHandler.SERVER_CONFIG.caveinRadius.get() * Math.sqrt(random.nextFloat());
+                double theta = random.nextFloat() * 2 * Math.PI;
+                int x = (int)(caveinOrigin.x + r * Math.cos(theta));
+                int y = ((int)caveinOrigin.y) - caveinSizeBelow;
+                int z = (int)(caveinOrigin.z + r * Math.sin(theta));
 
-                    FallingBlockEntity entity = FallingBlockEntity.fall(event.getServer().overworld(), currentBlockPos, event.getServer().overworld().getBlockState(currentBlockPos));
-                    entity.setHurtsEntities(1.0F, 4);
+                // Search bottom to top of cave in circle for blocks that have air or water under them
+                for(int iyx=0; iyx<caveinSizeBelow+caveinSizeAbove; iyx++){
+                    if(iyx+y < ServerConfigHandler.SERVER_CONFIG.maxCaveinYLevel.get()) {
+                        BlockPos block = new BlockPos(x, iyx + y, z);
+                        String blockStr = event.getServer().overworld().getBlockState(block).getBlock().toString();
 
-                    boolean foundPosition = false;
-                    for(int iex=0; iex<entitiesIndex; iex++){
-                        if(entities[iex] == null){
-                            entities[iex] = entity;
-                            foundPosition = true;
+                        BlockPos blockBelow = new BlockPos(x, iyx + y - 1, z);
+                        Material blockBelowMat = event.getServer().overworld().getBlockState(blockBelow).getMaterial();
+
+                        if (checkIfWhitelisted(blockStr) && (blockBelowMat == Material.AIR || blockBelowMat == Material.WATER)) {
+                            FallingBlockEntity entity = FallingBlockEntity.fall(event.getServer().overworld(), block, event.getServer().overworld().getBlockState(block));
+                            entities.add(entity);
                             break;
                         }
-                    }
-
-                    // If this doesn't work, I guess the block doesn't get sound when it drops, whatever
-                    if(!foundPosition && entitiesIndex < entities.length){
-                        entities[entitiesIndex] = entity;
-                        entitiesIndex++;
-                    }
-
-                    // Only increase falling block height if will not exceed limit
-                    BlockPos nextBlockPos = new BlockPos(currentBlockPos.getX(), currentBlockPos.getY()+1, currentBlockPos.getZ());
-                    String nextBlockPosStr = event.getServer().overworld().getBlockState(nextBlockPos).getBlock().toString();
-                    if(nextBlockPos.getY() < ServerConfigHandler.SERVER_CONFIG.maxCaveinYLevel.get() && checkIfWhitelisted(nextBlockPosStr)) {
-                        caveinBlockPositions[fallingBlockIndex] = nextBlockPos;
+                    }else{
+                        // Went above limit, stop searching
+                        break;
                     }
                 }
             }else{
